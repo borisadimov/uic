f9539947a7f2322c07cbe4c9c14288950fbcb781
Tom Roof <tcroof@gmail.com>
2018-04-26 02:04:31 -0400

    init commit // homepage

---
+++ pages/courses.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-container grid-list-xl>
+    <v-layout row>
+      <v-flex>
+        <h1>My Courses</h1>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs6>
+        <v-card class="course">
+          <v-card-text class="px-0">Cavity Prevention</v-card-text>
+           <v-progress-linear v-model="valueDeterminate"></v-progress-linear>
+        </v-card>
+      </v-flex>
+      <v-flex xs6>
+        <v-card class="course">
+          <v-card-text class="px-0">Baby Teeth</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex xs6>
+        <v-card class="course">
+          <v-card-text class="px-0">Proper Brushing</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex xs6>
+        <v-card class="course">
+          <v-card-text class="px-0">Prenatal Oral Care</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex xs6>
+        <v-card class="course">
+          <v-card-text class="px-0">Sugary Sweets</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex xs6>
+        <v-card class="course">
+          <v-card-text class="px-0">Home Dental Care</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        valueDeterminate: 50
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+h1
+  font-size 30px
+  font-family 'Zilla Slab', serif
+  color rgba(255,255,255,1)
+  margin 1rem 0
+  font-weight 700
+h2
+  font-size 18px
+  font-family 'Zilla Slab', serif
+  color rgba(255,255,255,1)
+  font-weight 700
+.course
+  background-color: rgba(46, 74, 110, .5) !important
+  border-radius: 10px
+  min-height: 157px
+  box-shadow: none
+  padding 15px
+.card__text
+  font-size 18px
+  font-family 'Zilla Slab', serif
+  color rgba(255,255,255,1)
+  font-weight 700
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12 class="cards">
+        <img src="img/cards.png" class="home-card">
+      </v-flex>
+      <v-flex xs12>
+        <h1>Welcome</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing</p>
+        <v-btn class="get-started" large round>Get Started</v-btn>
+      </v-flex>
+      <v-flex xs12>
+        <a href="" class="login">Log In</a>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<style lang="stylus">
+.home-card
+  width 281px
+  display inherit
+  margin 70px auto 0 auto
+
+.cards
+  background url('/img/bubbles.png') no-repeat center
+  background-size 326px
+  margin 0 auto
+
+h1
+  font-size 30px
+  text-align center
+  font-family 'Zilla Slab', serif
+  color rgba(255,255,255,1)
+  margin 1rem 0
+
+p
+  text-align center
+  font-size 19px
+  color rgba(255,255,255,0.7)
+
+.btn
+  margin 0 auto
+  display table
+
+.get-started
+  background-color #278ab5 !important
+  border-radius 100px
+  font-family 'Zilla Slab', serif
+  letter-spacing 1px
+  margin-top 30px
+
+.login
+  font-size 18px
+  text-decoration underline
+  color rgba(255,255,255,0.7)
+  text-align center
+  display inherit
+  text-align center
+  margin-top 60px
+</style>
